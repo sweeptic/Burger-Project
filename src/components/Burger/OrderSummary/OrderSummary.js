01d1a78d9c366ab6e5f8757f873712af0bbe4b9e
Attila <59293104+sweeptic@users.noreply.github.com>
2020-04-24 21:21:37 +0200

    23. Implementing the Button Component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Aux from "./../../../hoc/Aux_";
+import Button from './../../UI/Button/Button';
+
+
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients).map((igkey) => {
@@ -16,8 +19,8 @@ const orderSummary = (props) => {
             <p>A delicious burger with the following ingredients:</p>
             <ul>{ingredientSummary}</ul>
             <p>Continue to Checkout?</p>
-            <button>CANCEL</button>
-            <button>CONTINUE</button>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
     );
 };
