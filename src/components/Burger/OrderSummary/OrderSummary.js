33c3b9b6cf5bdaf5ec2e0ff8078e3546c72ef220
Attila <59293104+sweeptic@users.noreply.github.com>
2020-04-24 18:50:44 +0200

    22. Adding a Custom Button Component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -15,7 +15,9 @@ const orderSummary = (props) => {
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>{ingredientSummary}</ul>
-            <p>Continue to checkout?</p>
+            <p>Continue to Checkout?</p>
+            <button>CANCEL</button>
+            <button>CONTINUE</button>
         </Aux>
     );
 };
