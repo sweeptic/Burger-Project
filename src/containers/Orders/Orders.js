eb993d0d14cee1ffc5936cf842cdd1ec19e3bd02
Attila <59293104+sweeptic@users.noreply.github.com>
2020-05-03 21:24:30 +0200

    12. Outputting the Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -6,7 +6,6 @@ import withErrorHandler from './../../hoc/withErrorHandler/withErrorHandler';
 
 
 class Orders extends Component {
-
     state = {
         orders: [],
         loading: true
@@ -17,29 +16,29 @@ class Orders extends Component {
             .then(res => {
                 const fetchedOrders = [];
                 for (let key in res.data) {
-                    fetchedOrders.push(
-                        {
-                            ...res.data[key],
-                            id: key
-                        }
-                    )
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key  
+                    });
                 }
-                this.setState({ loading: false, orders: fetchedOrders })
-
+                this.setState({loading: false, orders: fetchedOrders});
             })
             .catch(err => {
-                this.setState({ loading: false })
-            })
-
+                this.setState({loading: false});
+            });
     }
 
-    render() {
+    render () {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
+                ))}
             </div>
-        )
+        );
     }
 }
 
