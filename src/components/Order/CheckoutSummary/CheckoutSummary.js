ea8cff0bc6f5c34ab02e79a210982b67df65f904
Attila <59293104+sweeptic@users.noreply.github.com>
2020-05-03 17:52:57 +0200

    5. Navigating Back & To Next Page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -12,8 +12,8 @@ const CheckoutSummary = (props) => {
             <div style={{ width: '100%', margin: 'auto' }}>
                 <Burger ingredients={ props.ingredients } />
             </div>
-            <Button  btnType="Danger">CANCEL</Button>
-            <Button  btnType="Success">CONTINUE</Button>
+            <Button clicked={props.checkoutCancelled} btnType="Danger">CANCEL</Button>
+            <Button clicked={props.checkoutContinued} btnType="Success">CONTINUE</Button>
         </div>
     )
 }
